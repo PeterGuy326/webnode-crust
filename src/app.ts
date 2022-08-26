de73a61b85935b179660d8e4ea3bd9390d14330b
FallingKids <739820174@qq.com>
2022-08-27 01:00:24 +0800

    定义框架路由模式 - /application/action/method

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as Koa from 'koa'
 import { router } from './routes'
+
 const app = new Koa()
 
 app.use(router.routes())
--- src/utils/route_func.ts
+++ src/utils/route_func.ts
@@ -1,11 +1,38 @@
-function getApplicationKey(): string {
-    return ''
+import * as path from 'path'
+import * as fs from 'fs'
+
+/**
+ * 获取根项目名
+ * @returns application
+ */
+function getApplicationName(): string {
+    const name = path.join(__dirname, '../../').split('/').pop()
+    const application = name.split('-').pop()
+    return application
 }
 
-function getActionKey(): string {
-    return ''
+/**
+ * 获取 action 列表
+ * @returns 
+ */
+function getActionList(): string[] {
+    var actionList = fs.readdirSync("../page")
+    actionList.map(action => {
+        action.replaceAll('.ts', '')
+    })
+    return actionList
 }
 
-function getMethodKey(): string {
-    return ''
+/**
+ * 根据 action 获取 方法列表
+ * @returns 
+ */
+function getMethodList(action: string): { [action: string]: string[]} {
+    return {}
+}
+
+export {
+    getApplicationName,
+    getActionList,
+    getMethodList
 }
